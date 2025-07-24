154a1321642e4fd95d6231a24e0cba234366f352
Adninjas <victor110800@gmail.com>
2025-07-24 18:26:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,4 @@ def recuperar_senha():
     }), 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
-
