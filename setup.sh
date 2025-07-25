0303ba063fd569415f7d751639ddeae7c04376c0
Adninjas <victor110800@gmail.com>
2025-07-25 01:02:55 -0400

    Corrigir setup.sh removendo apt-get e instalando apenas playwright

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Atualiza pacotes
-apt-get update
-
-# Instala dependências de bibliotecas gráficas que o Chromium precisa no Linux
-apt-get install -y wget gnupg libnss3 libatk-bridge2.0-0 libgtk-3-0 libxss1 libasound2 libgbm-dev libxshmfence1 libxrandr2 libxdamage1 libxcomposite1 libxcursor1 libxi6 libxtst6
-
-# Instala o Playwright e os navegadores com todas as dependências
+# Instala os navegadores e dependências via Playwright (sem apt-get)
 npm install playwright
 npx playwright install --with-deps
