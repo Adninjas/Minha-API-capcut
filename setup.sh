be32d61726050143d0be87b56b5e15cff900a10b
Adninjas <victor110800@gmail.com>
2025-07-25 00:25:22 -0400

    Corrigir caminho do ChromeDriver e atualizar setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
-set -e
-set -x
-npx playwright install
+
+# Instalar o Chrome e o ChromeDriver
+apt-get update
+apt-get install -y wget unzip curl gnupg
+
+# Instala o Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb || apt --fix-broken install -y
+
+# Baixa o ChromeDriver compatível com o Chrome instalado
+CHROME_VERSION=$(google-chrome --version | grep -oP '[0-9.]+' | head -1 | cut -d. -f1)
+CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
+wget https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+mv chromedriver /usr/local/bin/chromedriver
+chmod +x /usr/local/bin/chromedriver
+
+# Instala dependências Python
+pip install -r requirements.txt
