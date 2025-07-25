f145c16e299a68f21d9c0903ff05ff9ac2536a85
Adninjas <victor110800@gmail.com>
2025-07-25 00:57:54 -0400

    Corrigir permissões e setup.sh com --with-deps

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Instala dependências necessárias para Playwright
+# Atualiza pacotes
 apt-get update
+
+# Instala dependências de bibliotecas gráficas que o Chromium precisa no Linux
 apt-get install -y wget gnupg libnss3 libatk-bridge2.0-0 libgtk-3-0 libxss1 libasound2 libgbm-dev libxshmfence1 libxrandr2 libxdamage1 libxcomposite1 libxcursor1 libxi6 libxtst6
 
-# Instala Playwright e os browsers necessários
+# Instala o Playwright e os navegadores com todas as dependências
 npm install playwright
-npx playwright install
+npx playwright install --with-deps
